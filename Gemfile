e199c67edeb97f2a3e937ed6a5719a4f6fe6b474
teskew <teskinder1@gmail.com>
2021-03-09 21:04:59 -0600

    commit tree 907c73903a97c0e6b9fe49f8eb661305e344fec4

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
-# gem "rails"
+ gem "rails"
 
 gem 'sinatra', '~> 2.0.2'
     gem 'rake'
--- app.rb
+++ app.rb
@@ -2,13 +2,17 @@ require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
 
 class App < Sinatra::Base
-  get '/' do
-    erb :index
-  end
 
-  post '/' do
-    text_from_user = params[:user_text]
+get '/' do
+  erb :index
+end
 
-    erb :results
-  end
+
+post '/' do
+  text_from_user = params[:user_text]
+
+  @analyzed_text = TextAnalyzer.new(text_from_user)
+
+  erb :results
+end
 end
